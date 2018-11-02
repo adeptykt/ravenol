173b5987283661e273cc99aaf95488c14bbbf0ee
AdepT <m135et@gmail.com>
2018-11-02 14:52:22 +0900

    api server log

--- api/index.js
+++ api/index.js
@@ -4,8 +4,10 @@ import auth from '@feathersjs/authentication-client'
 import feathersStorage from '@/utils/feathers-storage'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3030', { transports: ['websocket'] })
+const server = process.env.API_URI || 'http://localhost:3030'
+const socket = io(server, { transports: ['websocket'] })
 // const socket = io('https://ravenol-server.herokuapp.com', { transports: ['websocket'] })
+console.log('Api server', server, process.env.API_URI);
 
 const feathersClient = feathers()
   .configure(socketio(socket))
